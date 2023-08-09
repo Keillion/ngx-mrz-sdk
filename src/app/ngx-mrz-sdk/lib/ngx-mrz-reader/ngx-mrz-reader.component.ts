60daee831509762251c9c337279ca048dae7248a
Keillion <735699921@qq.com>
2023-08-09 17:17:50 +0800

    mrz 4 direction

--- src/app/ngx-mrz-sdk/lib/ngx-mrz-reader/ngx-mrz-reader.component.ts
+++ src/app/ngx-mrz-sdk/lib/ngx-mrz-reader/ngx-mrz-reader.component.ts
@@ -30,7 +30,6 @@ export class NgxMrzReaderComponent implements OnInit {
         this.isLoaded = true;
       };
       this.reader = await LabelRecognizer.createInstance();
-      await this.reader.updateRuntimeSettingsFromString("MRZ");
       
     })();
   }
@@ -51,9 +50,47 @@ export class NgxMrzReaderComponent implements OnInit {
             img.onload = (event: any) => {
               this.overlayManager.updateOverlay(img.width, img.height);
               if (this.reader) {
-                this.reader.recognize(file).then((results: any) => {
-                  let txts: any = [];
-                  try {
+                // We will try 3 angles. Normal direction, turn left 90 degrees, turn right 90 degrees, turn 180 degrees.
+                (async()=>{
+                  try{
+                    // Normal direction
+                    await this.reader.updateRuntimeSettingsFromString("MRZ");
+                    let results = await this.reader!.recognize(file);
+                    console.log('0th')
+                    let runtimeSettings;
+                    if(!results.length){
+                      runtimeSettings = JSON.parse(await this.reader!.outputRuntimeSettingsToString());
+                      // Turn left 90 degrees
+                      runtimeSettings.ReferenceRegionArray[0].Localization.FirstPoint = [0,100];
+                      runtimeSettings.ReferenceRegionArray[0].Localization.SecondPoint = [0,0];
+                      runtimeSettings.ReferenceRegionArray[0].Localization.ThirdPoint = [100,0];
+                      runtimeSettings.ReferenceRegionArray[0].Localization.FourthPoint = [100,100];
+                      await this.reader!.updateRuntimeSettingsFromString(JSON.stringify(runtimeSettings));
+                      results = await this.reader!.recognize(file);
+                      console.log('1st')
+                    }
+                    if(!results.length){
+                      // Turn right 90 degrees
+                      runtimeSettings.ReferenceRegionArray[0].Localization.FirstPoint = [100,0];
+                      runtimeSettings.ReferenceRegionArray[0].Localization.SecondPoint = [100,100];
+                      runtimeSettings.ReferenceRegionArray[0].Localization.ThirdPoint = [0,100];
+                      runtimeSettings.ReferenceRegionArray[0].Localization.FourthPoint = [0,0];
+                      await this.reader!.updateRuntimeSettingsFromString(JSON.stringify(runtimeSettings));
+                      results = await this.reader!.recognize(file);
+                      console.log('2rd')
+                    }
+                    if(!results.length){
+                      // Turn 180 degrees
+                      runtimeSettings.ReferenceRegionArray[0].Localization.FirstPoint = [100,100];
+                      runtimeSettings.ReferenceRegionArray[0].Localization.SecondPoint = [0,100];
+                      runtimeSettings.ReferenceRegionArray[0].Localization.ThirdPoint = [0,0];
+                      runtimeSettings.ReferenceRegionArray[0].Localization.FourthPoint = [100,0];
+                      await this.reader!.updateRuntimeSettingsFromString(JSON.stringify(runtimeSettings));
+                      results = await this.reader!.recognize(file);
+                      console.log('3rd')
+                    }
+                    // handle final results
+                    let txts: any = [];
                     if (results.length > 0) {
                       for (let result of results) {
                         for (let line of result.lineResults) {
@@ -64,19 +101,20 @@ export class NgxMrzReaderComponent implements OnInit {
                       
                       let parsedResults = "";
                       if (txts.length == 2) {
-                        parsedResults = MrzParser.parseTwoLines(txts[0], txts[1]);
+                        parsedResults = MrzParser.parseTwoLines(txts[0], txts[1]) || MrzParser.parseTwoLines(txts[1], txts[0]);
                       }
                       else if (txts.length == 3) {
-                        parsedResults = MrzParser.parseThreeLines(txts[0], txts[1], txts[2]);
+                        parsedResults = MrzParser.parseThreeLines(txts[0], txts[1], txts[2]) || MrzParser.parseThreeLines(txts[2], txts[1], txts[0]);
                       }
                       this.result.emit([txts.join('\n'), parsedResults]);
+                      console.log(JSON.stringify(txts));
                     } else {
                       this.result.emit(txts.join(''));
                     }
-                  } catch (e) {
+                  }catch(e){
                     alert(e);
                   }
-                });
+                })();
               }
             };
             img.src = event.target.result;
